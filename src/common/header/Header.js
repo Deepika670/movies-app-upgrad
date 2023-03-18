2522c4f92feeff4cdd9389b541aa67dd7469caeb
Deepika670 <deepika.mathangi7@gmail.com>
2023-03-19 00:26:56 +0530

    imported and changed the order of imports in header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import './Header.css';
-import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import { Link } from 'react-router-dom';
+import FormControl from '@material-ui/core/FormControl';
+import './Header.css';
 
 const modalStyles = {
   content: {
