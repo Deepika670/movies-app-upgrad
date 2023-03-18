974a58ed137fc40f856751ef36355ba4dae74b9d
Deepika670 <deepika.mathangi7@gmail.com>
2023-03-18 21:06:15 +0530

    modal login form details

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,5 +1,16 @@
-import { Button, Modal } from '@material-ui/core';
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  Modal,
+  Tab,
+  Tabs,
+  Typography,
+} from '@material-ui/core';
+import { Input } from '@material-ui/icons';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import logo from '../../assets/logo.svg';
 
@@ -16,14 +27,31 @@ const modalStyles = {
   },
 };
 
+const TabContainer = function (props) {
+  return (
+    <Typography
+      component='div'
+      style={{ padding: 0, textAlign: 'center' }}
+    >
+      {props.children}
+    </Typography>
+  );
+};
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 class Header extends Component {
   constructor() {
     super();
     this.state = {
       showModal: false,
-      value: 0,
+      tabValue: 0,
       usernameRequired: '',
       username: '',
+      loginPassword: '',
+      loginPasswordRequired: '',
       registrationSuccessful: false,
       isLoggedIn:
         sessionStorage.getItem('access-token') === null ? false : true,
@@ -34,7 +62,7 @@ class Header extends Component {
     alert('Login Modal');
     this.setState({
       showModal: true,
-      value: 0,
+      tabValue: 0,
     });
   };
 
@@ -51,6 +79,59 @@ class Header extends Component {
 
   onCloseModal = () => {
     this.setState({ showModal: false });
+  };
+
+  onChangeUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+
+  onChangeLoginPassword = (event) => {
+    this.setState({
+      loginPassword: event.target.value,
+    });
+  };
+
+  onChangeTab = (event, value) => {
+    this.setState({ tabValue: value });
+  };
+
+  handleLogin = () => {
+    this.state.username === ''
+      ? this.setState({ usernameRequired: 'dispBlock' })
+      : this.setState({ usernameRequired: 'dispNone' });
+
+    this.state.loginPassword === ''
+      ? this.setState({ loginPasswordRequired: 'dispBlock' })
+      : this.setState({ loginPasswordRequired: 'dispNone' });
+
+    let loginData = null;
+    let xhrLogin = new XMLHttpRequest();
+    let that = this;
+
+    xhrLogin.addEventListener('readystatechange', function () {
+      if (this.readyState === 4) {
+        sessionStorage.setItem('uuid', JSON.parse(this.responseText).id);
+        sessionStorage.setItem(
+          'access-token',
+          xhrLogin.getResponseHeader('access-token')
+        );
+
+        that.setState({
+          isLoggedIn: true,
+        });
+
+        that.onCloseModal();
+      }
+    });
+
+    xhrLogin.open('POST', this.props.baseUrl + 'auth/login');
+    xhrLogin.setRequestHeader(
+      'Authorization',
+      'Basic ' +
+        window.btoa(this.state.username + ':' + this.state.loginPassword)
+    );
   };
 
   render() {
@@ -93,14 +174,74 @@ class Header extends Component {
             </Button>
           </div>
         </header>
-        <Modal
-          contentLable='Login'
+        {/* <Modal
+          contentLabel='Login'
           style={modalStyles}
           onRequestClose={this.onCloseModal}
           isOpen={this.state.showModal}
+          ariaHideApp={false}
+        ></Modal> */}
+        <Tabs
+          className='tabs'
+          value={this.state.tabValue}
+          onChange={this.onChangeTab}
         >
-          <div>Modal COntent</div>
-        </Modal>
+          <Tab label='Login' />
+          <Tab label='Register' />
+        </Tabs>
+
+        {this.state.value === 0 && (
+          <TabContainer>
+            <FormControl required>
+              <InputLabel htnlFor='username'>Username</InputLabel>
+              <Input
+                id='username'
+                type='text'
+                username={this.state.username}
+                onChange={this.onChangeUsername}
+              />
+              <FormHelperText className={this.state.usernameRequired}>
+                <span className='red'>required</span>
+              </FormHelperText>
+            </FormControl>
+
+            <br />
+            <br />
+
+            <FormControl required>
+              <InputLabel htmlFor='loginPassword'>Password</InputLabel>
+              <Input
+                id='loginPassword'
+                type='password'
+                loginPassword={this.state.loginPassword}
+                onChange={this.onChangeLoginPassword}
+              />
+              <FormHelperText className={this.state.loginPasswordRequired}>
+                <span className='red'>required</span>
+              </FormHelperText>
+            </FormControl>
+
+            <br />
+            <br />
+
+            {this.state.isLoggedIn === true && (
+              <FormControl>
+                <span className=''>Login Successful!</span>
+              </FormControl>
+            )}
+
+            <br />
+            <br />
+
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={this.handleLogin}
+            >
+              LOGIN
+            </Button>
+          </TabContainer>
+        )}
       </div>
     );
   }
