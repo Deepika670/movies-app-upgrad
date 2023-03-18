6d126159de36e65d0c7b8c3bb45a2dd584685b50
Deepika670 <deepika.mathangi7@gmail.com>
2023-03-19 00:43:59 +0530

    home page screen intial code with all the required fields

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,21 +1,22 @@
-import {
-  Button,
-  Card,
-  CardContent,
-  Checkbox,
-  FormControl,
-  Input,
-  InputLabel,
-  ListItemText,
-  MenuItem,
-  Select,
-  TextField,
-  Typography,
-} from '@material-ui/core';
-import { CheckBox } from '@material-ui/icons';
 import React, { Component } from 'react';
-import Header from '../../common/header/Header';
 import './Home.css';
+import Header from '../../common/header/Header';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Checkbox from '@material-ui/core/Checkbox';
+import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const styles = (theme) => ({
   root: {
@@ -28,19 +29,19 @@ const styles = (theme) => ({
     textAlign: 'center',
     fontSize: '1rem',
   },
-  upcomingMoviesListGird: {
+  gridListUpcomingMovies: {
     width: '100%',
-    flexWrap: 'nowrap',
     transform: 'translateZ(0)',
+    flexWrap: 'nowrap',
   },
-  mainList: {
+  gridListMain: {
     cursor: 'pointer',
     transform: 'translateZ(0)',
   },
-  FormControl: {
+  formControl: {
+    margin: theme.spacing.unit,
     minWidth: 240,
     maxWidth: 240,
-    margin: theme.spacing.unit,
   },
   title: {
     color: theme.palette.primary.light,
@@ -54,22 +55,23 @@ class Home extends Component {
       movieName: '',
       upcomingMovies: [],
       releasedMovies: [],
+      genres: [],
+      artists: [],
+      genresList: [],
+      artistsList: [],
       releaseDateStart: '',
       releaseDateEnd: '',
-      genres: [],
-      genresList: [],
-      artists: [],
-      artistsList: [],
     };
   }
 
   componentWillMount() {
+    // Get upcoming movies
     let data = null;
     let xhr = new XMLHttpRequest();
-    let currentState = this;
+    let that = this;
     xhr.addEventListener('readystatechange', function () {
       if (this.readyState === 4) {
-        currentState.setState({
+        that.setState({
           upcomingMovies: JSON.parse(this.responseText).movies,
         });
       }
@@ -77,12 +79,14 @@ class Home extends Component {
 
     xhr.open('GET', this.props.baseUrl + 'movies?status=PUBLISHED');
     xhr.setRequestHeader('Cache-Control', 'no-cache');
+    xhr.send(data);
 
+    // Get released movies
     let dataReleased = null;
     let xhrReleased = new XMLHttpRequest();
     xhrReleased.addEventListener('readystatechange', function () {
       if (this.readyState === 4) {
-        currentState.setState({
+        that.setState({
           releasedMovies: JSON.parse(this.responseText).movies,
         });
       }
@@ -92,11 +96,12 @@ class Home extends Component {
     xhrReleased.setRequestHeader('Cache-Control', 'no-cache');
     xhrReleased.send(dataReleased);
 
+    // Get filters
     let dataGenres = null;
     let xhrGenres = new XMLHttpRequest();
     xhrGenres.addEventListener('readystatechange', function () {
       if (this.readyState === 4) {
-        currentState.setState({
+        that.setState({
           genresList: JSON.parse(this.responseText).genres,
         });
       }
@@ -106,11 +111,12 @@ class Home extends Component {
     xhrGenres.setRequestHeader('Cache-Control', 'no-cache');
     xhrGenres.send(dataGenres);
 
+    // Get artists
     let dataArtists = null;
     let xhrArtists = new XMLHttpRequest();
     xhrArtists.addEventListener('readystatechange', function () {
       if (this.readyState === 4) {
-        currentState.setState({
+        that.setState({
           artistsList: JSON.parse(this.responseText).artists,
         });
       }
@@ -121,99 +127,231 @@ class Home extends Component {
     xhrArtists.send(dataArtists);
   }
 
+  movieNameChangeHandler = (event) => {
+    this.setState({ movieName: event.target.value });
+  };
+
+  genreSelectHandler = (event) => {
+    this.setState({ genres: event.target.value });
+  };
+
+  artistSelectHandler = (event) => {
+    this.setState({ artists: event.target.value });
+  };
+
+  releaseDateStartHandler = (event) => {
+    this.setState({ releaseDateStart: event.target.value });
+  };
+
+  releaseDateEndHandler = (event) => {
+    this.setState({ releaseDateEnd: event.target.value });
+  };
+
+  movieClickHandler = (movieId) => {
+    this.props.history.push('/movie/' + movieId);
+  };
+
+  filterApplyHandler = () => {
+    let queryString = '?status=RELEASED';
+    if (this.state.movieName !== '') {
+      queryString += '&title=' + this.state.movieName;
+    }
+    if (this.state.genres.length > 0) {
+      queryString += '&genres=' + this.state.genres.toString();
+    }
+    if (this.state.artists.length > 0) {
+      queryString += '&artists=' + this.state.artists.toString();
+    }
+    if (this.state.releaseDateStart !== '') {
+      queryString += '&start_date=' + this.state.releaseDateStart;
+    }
+    if (this.state.releaseDateEnd !== '') {
+      queryString += '&end_date=' + this.state.releaseDateEnd;
+    }
+
+    let that = this;
+    let dataFilter = null;
+    let xhrFilter = new XMLHttpRequest();
+    xhrFilter.addEventListener('readystatechange', function () {
+      if (this.readyState === 4) {
+        that.setState({
+          releasedMovies: JSON.parse(this.responseText).movies,
+        });
+      }
+    });
+
+    xhrFilter.open(
+      'GET',
+      this.props.baseUrl + 'movies' + encodeURI(queryString)
+    );
+    xhrFilter.setRequestHeader('Cache-Control', 'no-cache');
+    xhrFilter.send(dataFilter);
+  };
+
   render() {
+    const { classes } = this.props;
     return (
       <div>
-        <Header />
+        <Header baseUrl={this.props.baseUrl} />
 
-        <div className='upcoming-movies-heading'>
+        <div className={classes.upcomingMoviesHeading}>
           <span>Upcoming Movies</span>
         </div>
 
-        <div className='movies-and-find-movies-container'>
-          <div className='find-movies-section'>
+        <GridList
+          cols={5}
+          className={classes.gridListUpcomingMovies}
+        >
+          {this.state.upcomingMovies.map((movie) => (
+            <GridListTile key={'upcoming' + movie.id}>
+              <img
+                src={movie.poster_url}
+                className='movie-poster'
+                alt={movie.title}
+              />
+              <GridListTileBar title={movie.title} />
+            </GridListTile>
+          ))}
+        </GridList>
+
+        <div className='flex-container'>
+          <div className='left'>
+            <GridList
+              cellHeight={350}
+              cols={4}
+              className={classes.gridListMain}
+            >
+              {this.state.releasedMovies.map((movie) => (
+                <GridListTile
+                  onClick={() => this.movieClickHandler(movie.id)}
+                  className='released-movie-grid-item'
+                  key={'grid' + movie.id}
+                >
+                  <img
+                    src={movie.poster_url}
+                    className='movie-poster'
+                    alt={movie.title}
+                  />
+                  <GridListTileBar
+                    title={movie.title}
+                    subtitle={
+                      <span>
+                        Release Date:{' '}
+                        {new Date(movie.release_date).toDateString()}
+                      </span>
+                    }
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </div>
+          <div className='right'>
             <Card>
               <CardContent>
-                <FormControl>
-                  <Typography className=''>FIND MOVIES BY:</Typography>
+                <FormControl className={classes.formControl}>
+                  <Typography
+                    className={classes.title}
+                    color='textSecondary'
+                  >
+                    FIND MOVIES BY:
+                  </Typography>
                 </FormControl>
 
-                <FormControl>
+                <FormControl className={classes.formControl}>
                   <InputLabel htmlFor='movieName'>Movie Name</InputLabel>
-                  <Input id='movieName' />
+                  <Input
+                    id='movieName'
+                    onChange={this.movieNameChangeHandler}
+                  />
                 </FormControl>
 
-                <FormControl>
-                  <InputLabel htmlFor='genres'>Genres</InputLabel>
-                  <Select multiple>
-                    {this.state.genresList.map((eachGenre) => (
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor='select-multiple-checkbox'>
+                    Genres
+                  </InputLabel>
+                  <Select
+                    multiple
+                    input={<Input id='select-multiple-checkbox-genre' />}
+                    renderValue={(selected) => selected.join(',')}
+                    value={this.state.genres}
+                    onChange={this.genreSelectHandler}
+                  >
+                    {this.state.genresList.map((genre) => (
                       <MenuItem
-                        key={eachGenre.id}
-                        value={eachGenre.genre}
+                        key={genre.id}
+                        value={genre.genre}
                       >
-                        <CheckBox
-                          checked={
-                            this.state.genres.indexOf(eachGenre.genre) > -1
-                          }
+                        <Checkbox
+                          checked={this.state.genres.indexOf(genre.genre) > -1}
                         />
-                        <ListItemText primary={eachGenre.genre} />
+                        <ListItemText primary={genre.genre} />
                       </MenuItem>
                     ))}
                   </Select>
                 </FormControl>
 
-                <FormControl>
-                  <InputLabel htmlFor='artists'>Artists</InputLabel>
-                  <Select multiple>
-                    {this.state.artistsList.map((eachArtist) => (
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor='select-multiple-checkbox'>
+                    Artists
+                  </InputLabel>
+                  <Select
+                    multiple
+                    input={<Input id='select-multiple-checkbox' />}
+                    renderValue={(selected) => selected.join(',')}
+                    value={this.state.artists}
+                    onChange={this.artistSelectHandler}
+                  >
+                    {this.state.artistsList.map((artist) => (
                       <MenuItem
-                        key={eachArtist.id}
-                        value={
-                          eachArtist.first_name + ' ' + eachArtist.last_name
-                        }
+                        key={artist.id}
+                        value={artist.first_name + ' ' + artist.last_name}
                       >
                         <Checkbox
                           checked={
                             this.state.artists.indexOf(
-                              eachArtist.first_name + ' ' + eachArtist.last_name
+                              artist.first_name + ' ' + artist.last_name
                             ) > -1
                           }
                         />
                         <ListItemText
-                          primary={
-                            eachArtist.first_name + ' ' + eachArtist.last_name
-                          }
+                          primary={artist.first_name + ' ' + artist.last_name}
                         />
                       </MenuItem>
                     ))}
                   </Select>
                 </FormControl>
 
-                <FormControl>
+                <FormControl className={classes.formControl}>
                   <TextField
                     id='releaseDateStart'
                     label='Release Date Start'
                     type='date'
-                    defaultValue={''}
+                    defaultValue=''
                     InputLabelProps={{ shrink: true }}
+                    onChange={this.releaseDateStartHandler}
                   />
                 </FormControl>
 
-                <FormControl>
+                <FormControl className={classes.formControl}>
                   <TextField
                     id='releaseDateEnd'
                     label='Release Date End'
                     type='date'
-                    defaultValue={''}
+                    defaultValue=''
                     InputLabelProps={{ shrink: true }}
+                    onChange={this.releaseDateEndHandler}
                   />
                 </FormControl>
-
                 <br />
                 <br />
-
-                <FormControl>
-                  <Button>APPLY</Button>
+                <FormControl className={classes.formControl}>
+                  <Button
+                    onClick={() => this.filterApplyHandler()}
+                    variant='contained'
+                    color='primary'
+                  >
+                    APPLY
+                  </Button>
                 </FormControl>
               </CardContent>
             </Card>
@@ -224,4 +362,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
