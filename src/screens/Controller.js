4fd114693b7853bcd1848dd0df3e1e9d17219e31
Deepika670 <deepika.mathangi7@gmail.com>
2023-03-18 13:26:19 +0530

    added home,details,header files

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,32 +1,52 @@
-import React from "react";
-import Home from "../screens/home/Home";
-import Details from "../screens/details/Details";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import BookShow from "../screens/bookshow/BookShow";
-import Confirmation from "../screens/confirmation/Confirmation";
+import React from 'react';
+import Home from '../screens/home/Home';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import BookShow from '../screens/bookshow/BookShow';
+import Confirmation from '../screens/confirmation/Confirmation';
+import Details from '../screens/details/Details';
 
 const Controller = () => {
-  const baseUrl = "/api/v1/";
+  const baseUrl = '/api/v1/';
 
   return (
     <Router>
-      <div className="main-container">
+      <div className='main-container'>
         <Route
           exact
-          path="/"
-          render={(props) => <Home {...props} baseUrl={baseUrl} />}
+          path='/'
+          render={(props) => (
+            <Home
+              {...props}
+              baseUrl={baseUrl}
+            />
+          )}
         />
         <Route
-          path="/movie/:id"
-          render={(props) => <Details {...props} baseUrl={baseUrl} />}
+          path='/movie/:id'
+          render={(props) => (
+            <Details
+              {...props}
+              baseUrl={baseUrl}
+            />
+          )}
         />
         <Route
-          path="/bookshow/:id"
-          render={(props) => <BookShow {...props} baseUrl={baseUrl} />}
+          path='/bookshow/:id'
+          render={(props) => (
+            <BookShow
+              {...props}
+              baseUrl={baseUrl}
+            />
+          )}
         />
         <Route
-          path="/confirm/:id"
-          render={(props) => <Confirmation {...props} baseUrl={baseUrl} />}
+          path='/confirm/:id'
+          render={(props) => (
+            <Confirmation
+              {...props}
+              baseUrl={baseUrl}
+            />
+          )}
         />
       </div>
     </Router>
---
+++ src/screens/details/Details.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return <div>Details</div>;
+  }
+}
+
+export default Details;
