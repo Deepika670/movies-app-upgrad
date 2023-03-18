9e47adde630cd60479b97d39d0cb1df53a67906c
Deepika670 <deepika.mathangi7@gmail.com>
2023-03-18 16:04:34 +0530

    added header buttons

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,3 +1,4 @@
+import { Button } from '@material-ui/core';
 import React, { Component } from 'react';
 
 import logo from '../../assets/logo.svg';
@@ -7,18 +8,68 @@ import './Header.css';
 class Header extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      showModal: false,
+      value: 0,
+      registrationSuccessful: false,
+      isLoggedIn:
+        sessionStorage.getItem('access-token') === null ? false : true,
+    };
   }
+
+  onClickLoginButton = () => {
+    alert('Login Modal');
+    this.setState({
+      showModal: true,
+      value: 0,
+    });
+  };
+
+  onClickLogoutButton = () => {
+    alert('Logout Successful');
+    this.setState({
+      isLoggedIn: false,
+    });
+  };
 
   render() {
     return (
-      <div className='header-container'>
+      <header className='header-container'>
         <img
           src={logo}
           alt='Movies App Logo'
           className='movies-app-logo'
         />
-      </div>
+        {!this.state.isLoggedIn ? (
+          <div className='login-logout-button'>
+            <Button
+              variant='contained'
+              color='default'
+              onClick={this.onClickLoginButton}
+            >
+              Login
+            </Button>
+          </div>
+        ) : (
+          <div className='login-logout-button'>
+            <Button
+              variant='contained'
+              color='default'
+              onClick={this.onClickLogoutButton}
+            >
+              Logout
+            </Button>
+          </div>
+        )}
+        <div className='book-show-button'>
+          <Button
+            variant='contained'
+            color='primary'
+          >
+            Book Show
+          </Button>
+        </div>
+      </header>
     );
   }
 }
