7f2cb3ac1f437ead402567802779b8982eb6c83c
Deepika670 <deepika.mathangi7@gmail.com>
2023-03-19 14:26:29 +0530

    added state for movie detail page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -4,7 +4,40 @@ class Details extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      movie: {
+        genres: [],
+        trailer_url: '',
+        artists: [],
+      },
+      starIcons: [
+        {
+          id: 1,
+          stateId: 'star1',
+          color: 'black',
+        },
+        {
+          id: 2,
+          stateId: 'star2',
+          color: 'black',
+        },
+        {
+          id: 3,
+          stateId: 'star3',
+          color: 'black',
+        },
+        {
+          id: 4,
+          stateId: 'star4',
+          color: 'black',
+        },
+        {
+          id: 5,
+          stateId: 'star5',
+          color: 'black',
+        },
+      ],
+    };
   }
 
   render() {
